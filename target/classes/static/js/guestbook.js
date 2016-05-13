3329bfb920992ac0ed60f92ef57e17ac2867494d
tholou <tholou4reel@yahoo.com>
2016-05-13 17:16:38 +0300

    Resolved issue #1

--- target/classes/static/js/guestbook.js
+++ target/classes/static/js/guestbook.js
@@ -65,6 +65,11 @@ window.onload = function () {
 
 function reply (id) {
 	 console.log(id);
+	 var Form = [
+	   	      { id: 'name', placeholder: 'Name Field', required: 'required' },
+		      { id: 'email', placeholder: 'Email Field', type:'email'},
+		      { id: 'comment', placeholder: 'Comment Field', required: 'required' }
+		    ]
 	  swal.withForm({
 	    title: 'GuestBook Reply Form',
 	    /*text: 'Any text that you consider useful for the form',*/
@@ -72,57 +77,90 @@ function reply (id) {
 	    confirmButtonColor: '#DD6B55',
 	    confirmButtonText: 'Submit Reply!',
 	    closeOnConfirm: true,
-	    formFields: [
-	      { id: 'name', placeholder: 'Name Field', required: 'required' },
-	      { id: 'email', placeholder: 'Email Field',type:'email'},
-	      { id: 'comment', placeholder: 'Comment Field', required: 'required' }
-	    ]
+	    formFields: Form
 	  }, function (isConfirm) {
 	    // do whatever you want with the form data
 		  //alert("yesssss");
 		  if(isConfirm){
 			  
-			swal.showInputError('Please fill both Name and Comment');
-			swal.showEmailError('Please enter a correct Email format');
+/*			// Setup form validation on the #primaryPostForm element
+              $.validate({
+
+                         errorElement: "label", // default is 'label'
+                         errorPlacement: function(error, element) {
+                         error.insertAfter(element);
+                     },
+
+                  // Specify the validation rules
+                  rules: [
+                          
+                	  {'name': this.swalForm.name, 'email': this.swalForm.email, 'comment': this.swalForm.comment }
+                  ],
+
+                  // Specify the validation error messages
+                  messages: {
+                	  this.swalForm.name: '<i class="fa fa-exclamation-circle rediconcolour"></i> ' + " Poor friend!! They have no name!! " + ' <i class="fa fa-exclamation-circle rediconcolour"></i> ',
+                	  this.swalForm.email: '<i class="fa fa-exclamation-circle rediconcolour"></i> ' + " Please use a valid Email Format... " + ' <i class="fa fa-exclamation-circle rediconcolour"></i> '
+                	  this.swalForm.comment: '<i class="fa fa-exclamation-circle rediconcolour"></i> ' + " Poor friend!! You need to put a comment!! " + ' <i class="fa fa-exclamation-circle rediconcolour"></i> ',
+                  }
+
+             }); // end contact-form validate
+*/			
+			
 			//swal.showEmailError()
               //$('#title').css('border-color','red')
-
-              if(this.swalForm.name == ""){
+			  var isError = false;
+              if(this.swalForm.name == "" || this.swalForm.email == "" ||  this.swalForm.comment == ""){
             	//How to display error here ??
-                  this.swalForm.name.showInputError('Please enter Name');
+            	  alert("An error occured, Kindly fill all form fields correctly!");
+            	  var isError = true;
+            	  //swal.showInputError('Please enter Name');
+                  //this.swalForm.name.showInputError('Please enter Name');
             	  
               }
               
-              if(this.swalForm.email == ""){
+             /* if(this.swalForm.email == ""){
               	//How to display error here ??
-                    this.swalForm.email.showEmailError('Please enter a correct Email format');
+            	  alert("Please enter a correct Email format");
+            	  var isError = true;
+            	  //swal.showInputError('Please enter a correct Email format');
+                   // this.swalForm.email.showEmailError('Please enter a correct Email format');
               	  
                 }
-              
+              */
 
-              if(this.swalForm.comment == ""){
+             /* if(this.swalForm.comment == ""){
+            	  alert("Please enter Coment");
+            	  var isError = true;
             	//How to display error here ??
-                  this.swalForm.comment.showInputError('Please enter comment');
+            	 // swal.showInputError('Please enter Coment');
+                  //this.swalForm.comment.showInputError('Please enter comment');
             	  
-              }             
+              }  */           
 
              /* if(this.swalForm.title != undefined && this.swalForm.title != "" &&
                   this.swalForm.comment != undefined && this.swalForm.comment != "")*/
-			  $.ajax({
-				  headers: {
-					 'Accept': 'application/json',
-					 'Content-Type': 'application/json'
-				  },
-					type	: 'POST',
-					cache	: false,
-					contentType: 'application/json',
-					url		: '/guestreply/'+id,
-					data	: JSON.stringify({'name': this.swalForm.name, 'email': this.swalForm.email, 'comment': this.swalForm.comment }),
-				    dataType: 'json',
-					success	: function(data) {
-						alert('successful')
-					}
-				});
+              if(isError){
+            	  //document.getElementById("clickMe").click();
+            	  return false;
+              }
+              else{
+				  $.ajax({
+					  headers: {
+						 'Accept': 'application/json',
+						 'Content-Type': 'application/json'
+					  },
+						type	: 'POST',
+						cache	: false,
+						contentType: 'application/json',
+						url		: '/guestreply/'+id,
+						data	: JSON.stringify({'name': this.swalForm.name, 'email': this.swalForm.email, 'comment': this.swalForm.comment }),
+					    dataType: 'json',
+						success	: function(data) {
+							alert('successful')
+						}
+					});
+              }
 		    console.log(this.swalForm) // { name: 'user name', nickname: 'what the user sends' }
 		  }
 		 
